3dd68b250009b40f8fbfdfb455bd4a3253c79dc4
LaithLOK <nasser.aidane@gmail.COM>
2022-05-28 22:38:04 +0200

    Add flash messages and move base layout to layouts folder

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -36,6 +36,8 @@ class PinsController extends AbstractController
         $em->persist($pin);
         $em->flush();
 
+        $this->addFlash('success','Pin successfully created !');
+
         return $this->redirectToRoute('app_home');
       }
 
@@ -62,6 +64,9 @@ class PinsController extends AbstractController
 
         $em->flush();
 
+        $this->addFlash('success','Pin successfully updated !');
+
+
         return $this->redirectToRoute('app_home');
       }
       return $this->render('pins/edit.html.twig',[
@@ -79,6 +84,9 @@ class PinsController extends AbstractController
       if ($this->isCsrfTokenValid('pin_deletion_' .$pin->getId(),$request->request->get('Csrf_token'))) {
          $em ->remove($pin);
          $em->flush();
+
+        $this->addFlash('info','Pin successfully deleted !');
+
       }
 
       return $this->redirectToRoute('app_home');
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -6,6 +6,7 @@ namespace App\Entity;
 use App\Repository\PinRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 
@@ -21,9 +22,13 @@ class Pin
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message:"Title can't be blank")]
+    #[Assert\Length(min:3)]
     private $title;
 
     #[ORM\Column(type: 'text')]
+    #[Assert\NotBlank]
+    #[Assert\Length(min:10)]
     private $description;
 
 
